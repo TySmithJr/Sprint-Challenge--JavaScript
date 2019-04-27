1b52693e11931d1f32e601687c54d8d697a7649d
Tyrone Smith <billionairefly.ts@gmail.com>
2019-04-26 21:51:41 -0400

    finished sprint

--- challenges/functions.js
+++ challenges/functions.js
@@ -27,16 +27,16 @@ function greeting (firstName, lastName) {
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: becuase it is in the scope of the function which the value was stored for MediaKeySystemAccess.
 
 
 const external = "I'm outside the function";
